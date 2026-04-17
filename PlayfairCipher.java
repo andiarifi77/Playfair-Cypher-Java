e4a8b09f055b9fe220d4754a064332201434d594
AltinSollova7 <altin.sollova@student.uni-pr.edu>
2026-04-18 00:01:13 +0200

    Update PlayfairCipher.java

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -24,7 +24,18 @@ public class PlayfairCipher {
     }
 
 
+private int[] findPosition(char c) {
+    if (c == 'J') c = 'I';
 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (matrix[i][j] == c) {
+                return new int[]{i, j};
+            }
+        }
+    }
+    return null;
+}
 
 
 
