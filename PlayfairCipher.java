cf98e9f24dcf7cabe9320e44eb06bb493bcc4041
AltinSollova7 <altin.sollova@student.uni-pr.edu>
2026-04-18 22:00:53 +0200

    Update PlayfairCipher.java

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -14,3 +14,26 @@ private int[] findPosition(char c) {
 }
 commit 2 private List<
 
+private List<String> prepareText(String text) {
+    text = text.toUpperCase().replaceAll("[^A-Z]", "").replace("J", "I");
+    List<String> pairs = new ArrayList<>();
+
+    for (int i = 0; i < text.length(); i += 2) {
+        char a = text.charAt(i);
+        char b;
+
+        if (i + 1 < text.length()) {
+            b = text.charAt(i + 1);
+            if (a == b) {
+                b = 'X';
+                i--;
+            }
+        } else {
+            b = 'X';
+        }
+
+        pairs.add("" + a + b);
+    }
+
+    return pairs;
+}
